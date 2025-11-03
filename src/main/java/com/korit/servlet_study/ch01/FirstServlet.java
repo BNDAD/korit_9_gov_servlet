5c57af28592b948b02ece87ac2c31b6208bf6268
BNDAD <ral7057@naver.com>
2025-11-03 17:41:40 +0900

    Feat: Sevlet

--- src/main/java/com/korit/servlet_study/ch01/FirstServlet.java
+++ src/main/java/com/korit/servlet_study/ch01/FirstServlet.java
@@ -9,17 +9,6 @@ import java.io.IOException;
 
 public class FirstServlet extends HttpServlet {
 
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("요청 들어옴");
-    }
-
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("서비스 메서드 요청 들어옴");
-    }
-
     public FirstServlet() {
         System.out.println("FirstServlet 생성자 호출");
     }
@@ -28,11 +17,22 @@ public class FirstServlet extends HttpServlet {
     public void init(ServletConfig config) throws ServletException {
         System.out.println("초기화");
         config.getServletContext().setAttribute("age", 32);
-
     }
 
     @Override
     public void destroy() {
         System.out.println("소멸");
     }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("서비스 메서드 요청 들어옴");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("요청 들어옴");
+    }
+
+
 }
